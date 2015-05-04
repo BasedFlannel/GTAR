a395f94fbddf00a5eec8da0a934240d1633baef2
Cdiddy265 <collinfranzen@gmail.com>
2015-05-03 22:23:34 -0500

    Now with GUI!
    -GUI Completely designed, but not functional.
    -Added Documentation
    -Moved several blocks of code into private functions to reduce clutter
    in the constructor.

--- src/GTARFrame.java
+++ src/GTARFrame.java
@@ -1,10 +1,13 @@
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
+import java.awt.Insets;
 import java.util.ArrayList;
 
 import javax.swing.ButtonGroup;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JList;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
@@ -12,16 +15,14 @@ import javax.swing.JPanel;
 import javax.swing.JRadioButtonMenuItem;
 
 public class GTARFrame extends JFrame {
+	private static final long serialVersionUID = 7736399725583414552L;
 	/**
 	 * This is the primary panel the entire GUI is based off of.
 	 */
-	private static final long serialVersionUID = 7736399725583414552L;
-
 	public GTARFrame(){
 		super();
-		/**
-		 * Content Panel and GridBag
-		 */
+		
+		/**Content Panel and GridBag*/
 		//homePanel is the default content panel
 		JPanel homePanel = new JPanel(new GridBagLayout());
 		this.setContentPane(homePanel);
@@ -29,13 +30,63 @@ public class GTARFrame extends JFrame {
 		GridBagConstraints gbc = new GridBagConstraints();
 		gbc.fill = GridBagConstraints.BOTH;
 		
-		//Generic button to be used for multiple constructions later
-		JButton button;
-		/**
-		 * Menu Bar
-		 */
+		/**Menu Bar*/
 		//Menu Bar and top-level submenu declarations
 		JMenuBar menuBar = new JMenuBar();
+		populateMenuBar(menuBar);
+		this.setJMenuBar(menuBar);
+		/**Labels*/
+		JLabel lblSelectPlaylist = new JLabel("Select Playlist");
+		JLabel lblPlaylistContents = new JLabel("Playlist Contents");
+		JLabel lblInfo = new JLabel("Info");
+		
+		/**Buttons*/
+		JButton btnAddPlaylist = new JButton("Add Playlist");
+		
+		/**JLists*/
+		JList listPlaylists = new JList();
+		JList listContent = new JList();
+		JList listInfo = new JList();
+		
+		
+		/**Finally adding GUI elements to the Frame*/
+		//GBC constraints for getting all the UI in place.
+				gbc.insets = new Insets(3,3,0,3);
+				gbc.gridx = 1;
+				gbc.weightx=1.0;
+				gbc.weighty=0.0;
+				this.add(lblSelectPlaylist, gbc);
+				gbc.gridx=2;
+				this.add(lblPlaylistContents, gbc);
+				gbc.gridx=3;
+				this.add(lblInfo, gbc);
+				gbc.insets = new Insets(0,3,3,3);
+				gbc.gridx=1;
+				gbc.gridy=1;
+				gbc.weighty=1;
+				gbc.gridheight=2;
+				this.add(listPlaylists, gbc);
+				gbc.gridx=2;
+				this.add(listContent, gbc);
+				gbc.gridx=3;
+				this.add(listInfo, gbc);
+				gbc.gridx=0;
+				gbc.weightx=0;
+				gbc.weighty=0;
+				gbc.gridheight=1;
+				this.add(btnAddPlaylist, gbc);
+		
+		//muggling about 
+		//EOJ JPanel tasks
+		this.setBounds(50, 50, 900, 700);
+		this.setVisible(true);
+	}
+	/**
+	 * Method to populate the Menu Bar with Menus and items. Used to save space in the constructor.
+	 * @param menuBar The Menu Bar to alter
+	 */
+	private void populateMenuBar(JMenuBar menuBar){
+		//Menu declarations
 		JMenu menuFiles = new JMenu("File");
 		JMenu menuOptions = new JMenu("Options");
 		JMenu menuGames = new JMenu("Game");
@@ -53,7 +104,7 @@ public class GTARFrame extends JFrame {
 		GTARMenuItem menuGamesAddGameItem = new GTARMenuItem("Add Game");
 		
 		/**
-		 * delete this bit soon enough, filler for games menu.
+		 * Will eventually be replaced with code to read a file full of destination paths, and load those into the games Menu.
 		 */
 		ArrayList<String> tempGames = new ArrayList<String>();
 		tempGames.add("g1");
@@ -80,31 +131,18 @@ public class GTARFrame extends JFrame {
 		menuBar.add(menuFiles);
 		menuBar.add(menuOptions);
 		menuBar.add(menuGames);
-		
-		/*------------------------
-		*add menu bar to gridbag |
-		*-----------------------*/
-		gbc.weightx = 1;
-		gbc.gridwidth=3;
-		gbc.gridx = 0;
-		gbc.gridy = 0;
-		gbc.anchor = GridBagConstraints.FIRST_LINE_START;
-		homePanel.add(menuBar,gbc);
-		
-		/**
-		 * buttons
-		 */
-		button = new JButton("Button 3");
-		gbc.weightx=1;
-		gbc.weighty=1;
-		gbc.gridwidth=2;
-		gbc.gridx = 0;
-		gbc.gridy = 1;
-		gbc.ipady = 40;
-		homePanel.add(button, gbc);
-		
-		//EOJ JPanel tasks
-		this.setBounds(50, 50, 300, 300);
-		this.setVisible(true);
+	}
+	/**
+	 * Resets GridBagConstraints Parameters to defaults or something.
+	 * @param gbc The GridBagConstraints to reset
+	 */
+	private void resetGBC(GridBagConstraints gbc){
+		gbc.weightx=0.5;
+		gbc.weighty=0.5;
+		gbc.gridwidth=1;
+		gbc.gridheight=1;
+		gbc.ipadx=0;
+		gbc.ipady=0;
+		gbc.anchor=gbc.CENTER;
 	}
 }
